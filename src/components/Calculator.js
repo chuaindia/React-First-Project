3da1eb1a2c482463114444c5a9b1d33145b1b920
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-01 15:35:30 +0530

    Add Calculator Display files as instructed

---
+++ src/components/Calculator.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const buttons = ['AC', '+/-', '%', '÷', '7',
+  '8', '9', 'x', '4', '5', '6', '-',
+  '1', '2', '3', '+',
+  '0', '.', '-'];
+
+const MakeArray = () => {
+  const arrayForButtons = [];
+  buttons.forEach((button) => arrayForButtons.push(
+    <button className="calc-btns" type="button">{button}</button>,
+  ));
+  return arrayForButtons;
+};
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="calculator">
+        <div id="display-calculator" />
+        <div id="calculator-buttons">
+          <MakeArray />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
