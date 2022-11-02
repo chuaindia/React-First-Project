71b53cac29d3feec7f8f782af6c17bb7bba715a5
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-02 13:40:14 +0530

    backup for event management file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,43 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 const buttons = ['AC', '+/-', '%', '÷', '7',
   '8', '9', 'x', '4', '5', '6', '-',
   '1', '2', '3', '+',
   '0', '.', '='];
 
-const MakeArray = () => {
-  const arrayForButtons = [];
-  buttons.forEach((button) => arrayForButtons.push(
-    <button className="calc-btns" type="button">{button}</button>,
-  ));
-  return arrayForButtons;
-};
-
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.handleEvent = this.handleEvent.bind(this);
+    this.MakeArray = this.MakeArray.bind(this);
+    this.state = { total: '0', next: null, operation: null };
   }
 
+    handleEvent = (e) => {
+      this.setState((previousState) => calculate(previousState, e.target.innertext));
+    }
+
+  MakeArray = () => {
+    const arrayForButtons = [];
+    buttons.forEach((button) => arrayForButtons.push(
+      <button key={button} onClick={this.handleEvent} className="calc-btns" type="button">{button}</button>,
+    ));
+    return arrayForButtons;
+  };
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <>
         <div id="calculator">
           <div id="display-calculator" />
-          <input />
+          {total}
+          {operation}
+          {next}
         </div>
         <div id="calculator-buttons">
-          <MakeArray />
+          {this.MakeArray()}
         </div>
       </>
     );
