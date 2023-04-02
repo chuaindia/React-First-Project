0ab01a6ae499721b334c5601a83f6539585044fd
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-04-02 13:03:10 +0530

    Update content of home

---
+++ src/components/home.js
@@ -0,0 +1,31 @@
+import './home.css';
+
+const Home = () => (
+  <section className="home">
+    <h2>
+      Welcome to our page
+    </h2>
+    <p className="firstParagraph">
+    A mathemagician is a mathematician who is also a magician. The term "mathemagic" is believed to have been introduced by Royal Vale Heath with his 1933 book "Mathemagic".
+
+The name "mathemagician" was probably first applied to Martin Gardner, but has since been used to describe many mathematician/magicians, including Arthur T. Benjamin, Persi Diaconis, and Colm Mulcahy. Diaconis has suggested that the reason so many mathematicians are magicians is that "inventing a magic trick and inventing a theorem are very similar activities."
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Est id cupiditate perferendis. Dicta voluptatem, sunt voluptatibus esse
+      autem voluptas rerum iure vitae facere eum quo. Consequuntur numquam officia
+      provident molestiae.
+    </p>
+    <br />
+    <br />
+    <p>
+    Mathemagician is a neologism, specifically a portmanteau, that combines mathematician and magician. A great number of self-working mentalism tricks rely on mathematical principles. Max Maven often utilizes this type of magic in his performance.
+
+The Mathemagician is the name of a character in the 1961 children's book The Phantom Tollbooth. He is the ruler of Digitopolis, the kingdom of mathematics.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Est id cupiditate perferendis. Dicta voluptatem, sunt voluptatibus esse
+      autem voluptas rerum iure vitae facere eum quo. Consequuntur numquam officia
+      provident molestiae.
+    </p>
+  </section>
+);
+
+export default Home;
